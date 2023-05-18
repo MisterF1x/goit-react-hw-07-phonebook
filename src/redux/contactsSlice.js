4bf0c655ccf6de677e44cf161474ac2a0f363cea
X Mister <someinkognito@gmail.com>
2023-05-18 15:55:30 +0300

    comment

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -1,22 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { fetchContacts, addContact, deleteContact } from './operation';
-// import storage from 'redux-persist/lib/storage';
-// import { persistReducer } from 'redux-persist';
 
-// const initialContacts = [
-//   { id: 'id-1', name: 'Rosie Simpson', number: '+380-32-459-12-56' },
-//   { id: 'id-2', name: 'Hermione Kline', number: '+980-32-443-89-12' },
-//   { id: 'id-3', name: 'Eden Clements', number: '180-32-645-17-79' },
-//   { id: 'id-4', name: 'Annie Copeland', number: '380-32-227-91-26' },
-// ];
-// {
-//     contacts: {
-//       items: [],
-//       isLoading: false,
-//       error: null,
-//     },
-//     filter: '',
-//   }
 const handlePending = state => {
   state.isLoading = true;
 };
